aed92a8de5eefdb05625419eef60991ba27be9e0
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-06-03 16:38:15 +0900

    Add files via upload

--- MenuSelectStage.java
+++ MenuSelectStage.java
@@ -2,14 +2,18 @@ package menuselectstage;
 
 import static javax.swing.JOptionPane.*;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import datastage.DataStage;
 import mainframe.MainFrame;
 
 //ステージ選択画面
@@ -21,29 +25,22 @@ public class MenuSelectStage extends JPanel{
 	JButton hardModeButton = new JButton();
 	JScrollPane stageScroll = new JScrollPane();
 	JScrollPane enemyScroll = new JScrollPane();
+	JScrollPane meritScroll = new JScrollPane();
 	StagePanel StagePanel = new StagePanel();
+	MeritPanel MeritPanel = new MeritPanel();
 	EnemyPanel EnemyPanel = new EnemyPanel();
-	
-	
-	
-	
-	
-	
-	
-	
+	List<BufferedImage> stageImage = new DataStage().getStageImage(5);
 	
 	public MenuSelectStage(MainFrame MainFrame) {
+		setBackground(new Color(240, 170, 80));
 		addStageLabel();
 		addInformationLabel();
 		addReturnButton(MainFrame);
 		addNormalModeButton();
 		addHardModeButton();
 		addStageScroll();
+		addMeritScroll();
 		addEnemyScroll();
-		
-		
-		
-		
 	}
 	
 	protected void paintComponent(Graphics g) {
@@ -54,10 +51,8 @@ public class MenuSelectStage extends JPanel{
 		setNormalModeButton();
 		setHardModeButton();
 		setStageScroll();
+		setMeritScroll();
 		setEnemyScroll();
-		
-		
-		
 		drawField(g);
 		requestFocus();
 	}
@@ -95,38 +90,38 @@ public class MenuSelectStage extends JPanel{
 	
 	private void setReturnButton() {
 		returnButton.setText("戻る");
-		returnButton.setBounds(10, 450, 200, 60);
+		returnButton.setBounds(10, 460, 200, 60);
 		setButton(returnButton);
 	}
 	
 	private void addNormalModeButton() {
 		add(normalModeButton);
 		normalModeButton.addActionListener(e->{
-			showMessageDialog(null, "調整中　narmal mode でゲーム開始");
+			showMessageDialog(null, "調整中\nnormal mode でゲーム開始");
 		});
 	}
 	
 	private void setNormalModeButton() {
 		normalModeButton.setText("normal");
-		normalModeButton.setBounds(320, 450, 100, 60);
+		normalModeButton.setBounds(630, 460, 155, 60);
 		setButton(normalModeButton);
 	}
 	
 	private void addHardModeButton() {
 		add(hardModeButton);
 		hardModeButton.addActionListener(e->{
-			showMessageDialog(null, "調整中　hard mode でゲーム開始");
+			showMessageDialog(null, "調整中\nhard mode でゲーム開始");
 		});
 	}
 	
 	private void setHardModeButton() {
 		hardModeButton.setText("hard");
-		hardModeButton.setBounds(430, 450, 100, 60);
+		hardModeButton.setBounds(795, 460, 155, 60);
 		setButton(hardModeButton);
 	}
 	
 	private void setButton(JButton button) {
-		button.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 15));
+		button.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 20));
 	}
 	
 	private void addStageScroll() {
@@ -135,8 +130,18 @@ public class MenuSelectStage extends JPanel{
 	}
 	
 	private void setStageScroll() {
-		stageScroll.setBounds(10, 40, 200, 400);
+		stageScroll.setBounds(10, 40, 200, 410);
 		stageScroll.setPreferredSize(stageScroll.getSize());
+	}
+	
+	private void addMeritScroll() {
+		meritScroll.getViewport().setView(MeritPanel);
+    	add(meritScroll);
+	}
+	
+	private void setMeritScroll() {
+		meritScroll.setBounds(220, 275, 400, 245);
+		meritScroll.setPreferredSize(meritScroll.getSize());
 	}
 	
 	private void addEnemyScroll() {
@@ -145,22 +150,37 @@ public class MenuSelectStage extends JPanel{
 	}
 	
 	private void setEnemyScroll() {
-		enemyScroll.setBounds(220, 40, 320, 400);
+		enemyScroll.setBounds(630, 40, 320, 410);
 		enemyScroll.setPreferredSize(enemyScroll.getSize());
 	}
 	
-	
-	
-	
-	
-	
 	private void drawField(Graphics g){
 		//フィールド画像を表示
+		//現在の画像はテスト用
+		g.drawImage(stageImage.get(0), 220, 40, this);
 	}
 }
 
 //ステージ切り替え
 class StagePanel extends JPanel{
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+//戦功情報
+class MeritPanel extends JPanel{
+	
+	
+	
+	
+	
 	
 	
 	
