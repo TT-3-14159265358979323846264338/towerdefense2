e83c3253a4b990c2b7fd5405401584e02b4e01a3
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-07-16 15:08:05 +0900

    Add files via upload

--- MainFrame.java
+++ MainFrame.java
@@ -1,8 +1,17 @@
 package defendthecastle;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import battle.Battle;
 import defaultdata.stage.StageData;
@@ -17,50 +26,101 @@ public class MainFrame extends JFrame{
 	
 	protected void mainMenuDraw() {
 		getContentPane().removeAll();
+		add(new MenuMain(this));
 		setTitle("メインメニュー");
 		setSize(585, 510);
-		add(new MenuMain(this));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 	
 	protected void itemGetMenuDraw() {
 		getContentPane().removeAll();
+		add(new MenuItemGet(this));
 		setTitle("ガチャ");
 		setSize(585, 510);
-		add(new MenuItemGet(this));
 		setLocationRelativeTo(null);
 	}
 	
 	protected void itemDisposeMenuDraw() {
 		getContentPane().removeAll();
+		add(new MenuItemDispose(this));
 		setTitle("リサイクル");
 		setSize(715, 640);
-		add(new MenuItemDispose(this));
 		setLocationRelativeTo(null);
 	}
 	
 	protected void compositionDraw() {
 		getContentPane().removeAll();
+		add(new MenuComposition(this));
 		setTitle("ユニット編成");
 		setSize(975, 570);
-		add(new MenuComposition(this));
 		setLocationRelativeTo(null);
 	}
 	
 	public void selectStageDraw() {
 		getContentPane().removeAll();
+		add(new MenuSelectStage(this));
 		setTitle("ステージ選択");
 		setSize(925, 570);
-		add(new MenuSelectStage(this));
 		setLocationRelativeTo(null);
 	}
 	
 	public void battleDraw(StageData StageData, List<Boolean> clearMerit, int difficultyCode) {
 		getContentPane().removeAll();
+		add(new Battle(this, StageData, clearMerit, difficultyCode));
 		setTitle(StageData.getName());
 		setSize(1235, 600);
-		add(new Battle(this, StageData, clearMerit, difficultyCode));
 		setLocationRelativeTo(null);
+	}
+}
+
+//ロード画面
+class LoadFrame extends JFrame{
+	Dimension size;
+	
+	protected LoadFrame(Dimension size) {
+		this.size = size;
+	}
+	
+	protected void frame() {
+		setTitle("ロード");
+		setSize(size);
+		add(new LoadPanel(size));
+		setLocationRelativeTo(null);
+	}
+}
+
+class LoadPanel extends JPanel implements ActionListener{
+	JLabel load = new JLabel();
+	Dimension size;
+	Timer timer = new Timer(100, this);
+	int alpha = 255;
+	boolean canDecrease;
+	
+	protected LoadPanel(Dimension size) {
+		setBackground(Color.BLACK);
+		this.size = size;
+		load.setText("ロード中");
+		add(load);
+		timer.start();
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		load.setBounds((int) (size.getWidth() - 200), (int) (size.getHeight() - 100), 400, 40);
+		load.setForeground(new Color(255, 0, 0, alpha));
+		load.setFont(new Font("Arail", Font.BOLD, 30));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(alpha <= 100 || 255 <= alpha) {
+			canDecrease = canDecrease? false: true;
+		}
+		if(canDecrease) {
+			alpha -= 10;
+		}else {
+			alpha += 10;
+		}
 	}
 }
